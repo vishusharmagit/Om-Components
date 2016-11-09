470909317e9c457e9cc9e05c8fa7c2cfc8893a02
Vishu <vishu@getperx.com>
2016-11-09 14:44:47 +0800

    Testing

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,12 @@ module.exports = function(defaults) {
     // Add options here
   });
 
+  // app.import('bower_components/bootstrap/dist/css/bootstrap.css');
+  // app.import('bower_components/bootstrap/dist/css/bootstrap-theme.css');
+  // app.import('bower_components/bootstrap/dist/js/bootstrap.js');
+
+
+
   /*
     This build file specifies the options for the dummy test app of this
     addon, located in `/tests/dummy`
